4c9c1ffadbaebb304dbe117d2864802996e4898d
Almario Miano <almariomiano@gmail.com>
2024-04-18 08:03:53 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -37,7 +37,7 @@
                     </div>
                     <!-- END CONTENT BODY -->
 
-        <?php include_once ('footer.php'); ?>
+        <?php include_once ('footer.php'); /* just a comment */ ?>
         
     </body>
 </html>

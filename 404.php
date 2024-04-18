f024b06c1ab369240496e3d9085c57199384b2f6
Almario Miano <almariomiano@gmail.com>
2024-04-18 07:14:59 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@
                         <div class="col-md-12 page-404">
                             <div class="number font-green"> 404 </div>
                             <div class="details">
-                                <h3>Oops! You're lost..</h3>
+                                <h3>Oops! You're lost.</h3>
                                 <p>
                                     We can not find the page you're looking for.<br/>
                                     <a href="dashboard"> Return home </a> or try the search bar below..

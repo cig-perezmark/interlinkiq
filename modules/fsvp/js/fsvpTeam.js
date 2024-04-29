ecaaa545496942f80fa184841f68c616735dc40f
Almario Miano <almario.dev@gmail.com>
2024-04-29 15:31:51 -0500

    Update fsvpTeam.js

--- modules/fsvp/js/fsvpTeam.js
+++ modules/fsvp/js/fsvpTeam.js
@@ -1,6 +1,74 @@
+const newMemberForm = $('#newMemberForm');
+let newMemberModalErrorTimeout = null;
+let fsvpTeamTable = null;
+
 $(document).ready(function() {
     initMemberSearch();
+    fsvpTeamTable = initDataTable($('#tableFSVPTeamRoster'))
 });
+
+// modal hide event hook 
+$('#modalAddMember').on('hidden.bs.modal', function() {
+    newMemberForm.find('[name=employee]').val('').trigger('change');
+    newMemberForm.find('[data-name]').val('');
+    newMemberForm.find('[data-title]').val('');
+    newMemberForm.find('[data-email]').val('');
+    newMemberForm.find('[data-phone]').val('');
+    newMemberForm.find('[data-avatar]').attr('src', 'https://via.placeholder.com/100x100/EFEFEF/AAAAAA.png?text=no+image');
+});
+
+newMemberForm.on('submit', function(e) {
+    e.preventDefault();
+    
+    const employee = e.target.employee.value;
+
+    if(!employee) {
+        if(newMemberModalErrorTimeout) {
+            clearTimeout(newMemberModalErrorTimeout);
+        }
+        
+        $('#modalNewMemberErrorMessage').text('Please select an employee to proceed!');
+        $('#modalNewMemberError').fadeIn('linear', () => {
+            newMemberModalErrorTimeout = setTimeout(() => {
+                $('#modalNewMemberError').fadeOut();
+                newMemberModalErrorTimeout = null;
+            }, 5000)
+        });
+
+        return;
+    }
+
+    const formData = new FormData(e.target);
+
+    $.ajax({
+        url: baseUrl + "newFSVPTeamMember",
+        type: "POST",
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function({id, message}) {
+            if(id) {
+                renderDTRow({
+                    name: newMemberForm.find('[data-name]').val(),
+                    position: newMemberForm.find('[data-title]').val(),
+                    phone: newMemberForm.find('[data-phone]').val(),
+                    email: newMemberForm.find('[data-email]').val(),
+                    id,
+                    type: e.target.member_type.value
+                }).draw()
+            }
+
+            $('#modalAddMember').modal('hide');
+            bootstrapGrowl(message || 'Saved!');
+        },
+        error: function() {
+            bootstrapGrowl('Error!');
+        },
+        complete: function() {
+            l.stop();
+        }
+    });
+})
 
 function initMemberSearch() {
     const searchEmpDropdown = $("#employeeSearchDd");
@@ -8,24 +76,22 @@ function initMemberSearch() {
         if (data.loading) return data.name;
 
         return `<div class="select2-result-repository clearfix">
-            <div class="select2-result-repository__avatar">
+            <div class="select2-result-repository__avatar" style="width: 40px;">
                 <img src="${data.avatar}" alt="Avatar" />
             </div>
-            <div class="select2-result-repository__meta"> 
+            <div class="select2-result-repository__meta" style="margin-left: 0;"> 
                 <div class="select2-result-repository__title">${data.name}</div>
-                <div class="select2-result-repository__description">Job ID${data.job_description_id}</div>
+                <div class="select2-result-repository__description">${data.position}</div>
             </div>
         </div>`;
     }
 
     function formatProductSelection(data) {
-        // __selectedProductData = data; // set the selected material id to the hidden input
-        // if (data.name) {
-        //     $(".addProductBtn").removeAttr("disabled");
-        // } else {
-        //     $(".addProductBtn").prop("disabled", true);
-        // }
-        console.log(data)
+        newMemberForm.find('[data-name]').val(data.name);
+        newMemberForm.find('[data-title]').val(data.position);
+        newMemberForm.find('[data-email]').val(data.email);
+        newMemberForm.find('[data-phone]').val(data.phone);
+        newMemberForm.find('[data-avatar]').attr('src', data.avatar);
         return data.name || data.text;
     }
 
@@ -58,5 +124,42 @@ function initMemberSearch() {
         minimumInputLength: 1,
         templateResult: formatProduct,
         templateSelection: formatProductSelection,
+    });
+}
+
+function renderDTRow(d) {
+    fsvpTeamTable.dt.row.add([
+        d.name,
+        d.position,
+        d.phone,
+        d.email,
+        ``,
+        ``,
+        `
+            <div class="d-flex center">
+                <button type="button" class="btn-link">Remove</button>
+            </div>
+        `,
+    ]);
+    return fsvpTeamTable.dt;
+}
+
+function fetchTeamRoster() {
+    $.ajax({
+        url: baseUrl + "getFSVPRoster",
+        type: "GET",
+        contentType: false,
+        processData: false,
+        success: function({data}) {
+            if(data) {
+                fsvpTeamTable.dt.clear().draw();
+                data.forEach((d) => renderDTRow(d));
+                fsvpTeamTable.dt.draw();
+                // fancyBoxes();      
+            }
+        },
+        error: function() {
+            bootstrapGrowl('Error fetching records!');
+        },
     });
 }

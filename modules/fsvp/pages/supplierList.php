1140e213db06ea756c738401c613603420166610
Almario Miano <almario.dev@gmail.com>
2024-05-02 16:50:07 -0500

    Update supplierList.php

--- modules/fsvp/pages/supplierList.php
+++ modules/fsvp/pages/supplierList.php
@@ -258,4 +258,24 @@ th {
     </div>
 </div>
 
+<div class="modal fade in" id="modalViewFiles" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title">New Supplier Form</h4>
+            </div>
+            <div class="modal-body form-body">
+                <p class="text-muted">
+                    Window for viewing files...
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
+                <button type="button" class="btn green">Submit </button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script defer src="modules/fsvp/js/supplierlist.js"></script>

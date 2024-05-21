fa44bd8939fdb41f6a719c38304566c136317e2d
Marvin Villasencio Baylon <147330296+MarvinBaylon07@users.noreply.github.com>
2024-05-21 16:02:58 -0500

    dynamic url

--- crm/unsubscribe.php
+++ crm/unsubscribe.php
@@ -1,5 +1,10 @@
 <?php
 require '../database.php';
+$pageUrl =  "http://" . $_SERVER['SERVER_NAME'];
+
+if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {
+	$pageUrl = "https://" . $_SERVER['SERVER_NAME'];
+}
 if (isset($_GET['token'])) {
     $method = "AES-256-CBC";
     $key = "interlink";
@@ -11,7 +16,7 @@ if (isset($_GET['token'])) {
         $stmt = $conn->prepare("UPDATE tbl_Customer_Relationship SET flag = 0 WHERE crm_id = ?");
         $stmt->bind_param('i', $decryptedToken);
         if ($stmt->execute()) {
-            header("Location: https://interlinkiq.com//subscribe-page?token=" . $_GET['token']);
+            header("Location: $pageUrl/subscribe-page?token=" . $_GET['token']);
             exit();
         } else {
             echo 'Error updating contact status.';

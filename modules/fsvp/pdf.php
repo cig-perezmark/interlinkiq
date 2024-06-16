2a8db92bdb54945fcafe8191084d5de872fdea58
Almario Miano <almariomiano@gmail.com>
2024-06-16 13:46:35 -0500

    added ipr pdf case

--- modules/fsvp/pdf.php
+++ modules/fsvp/pdf.php
@@ -37,6 +37,15 @@ if(isset($_GET['pdf'])) {
             
             include_once __DIR__ .'/pdfs/importer_info.php';
             break;
+        case 'ipr':
+            $recordId = $_GET['r'] ?? null;
+
+            if(empty($recordId)) {
+                die('Record not found.');
+            }
+            
+            include_once __DIR__ .'/pdfs/ingredient_product_register.php';
+            break;
         default:
             echo 'default';
             break;

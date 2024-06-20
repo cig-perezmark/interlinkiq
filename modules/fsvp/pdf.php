83cda49d874349847a8888be0996405b58dc711e
Almario Miano <almariomiano@gmail.com>
2024-06-20 12:01:21 -0500

    created template for pdf documents

--- modules/fsvp/pdf.php
+++ modules/fsvp/pdf.php
@@ -1,62 +1,84 @@
 <?php
 
+require_once __DIR__ . '/../../assets/TCPDF/tcpdf.php';
 include_once __DIR__ .'/utils.php';
-
-function txt($value, $default = 'N/A') {
-    return empty($value) ? $default : $value;
-}
 
 if(isset($_GET['pdf'])) {
     $pdfView = $_GET['pdf'];
-
-    switch ($pdfView) {
-        case 'evaluation_form':
-            $recordId = $_GET['r'] ?? null;
-
-            if(empty($recordId)) {
-                die('Record not found.');
-            }
-            
-            include_once __DIR__ .'/pdfs/evaluation_form.php';
-            break;
-        case 'cbp':
-            $recordId = $_GET['r'] ?? null;
-
-            if(empty($recordId)) {
-                die('Record not found.');
-            }
-            
-            include_once __DIR__ .'/pdfs/cbp_filing_form.php';
-            break;
-        case 'importer_information':
-            $recordId = $_GET['r'] ?? null;
-
-            if(empty($recordId)) {
-                die('Record not found.');
-            }
-            
-            include_once __DIR__ .'/pdfs/importer_info.php';
-            break;
-        case 'ipr':
-            $recordId = $_GET['r'] ?? null;
-
-            if(empty($recordId)) {
-                die('Record not found.');
-            }
-            
-            include_once __DIR__ .'/pdfs/ingredient_product_register.php';
-            break;
-        case 'activities-worksheet':
-            $recordId = $_GET['r'] ?? null;
-
-            if(empty($recordId)) {
-                die('Record not found.');
-            }
-            
-            include_once __DIR__ .'/pdfs/activities_worksheet.php';
-            break;
-        default:
-            echo 'default';
-            break;
+    $recordId = $_GET['r'] ?? null;
+    
+    if(empty($recordId)) {
+        die('Incorrect parameter(s).');
     }
+
+    $title = 'DOCUMENT TITLE';
+    $css = '
+        <style>
+            table { width: 100%; padding: 5px; }
+            td, th { border: 1px solid black;}
+        </style>
+    ';
+
+    // custom TCPDF class
+    class TCPDF2 extends TCPDF {
+        public function __construct() {
+            parent::__construct('P', 'pt', 'Letter');
+        }
+        // auto cleaning 
+        public function __destruct() {
+            $this->endPage();
+            $this->close();
+            parent::__destruct();
+        }
+        // override header 
+        public function Header() {
+            // $pageNo = $this->getAliasNumPage();
+            // $totalPage = $this->getAliasNbPages();
+            // $this->SetY(20);
+            $html = '
+                <table width="100%">
+                    <tr>
+                        <td style="color:gray;">Header</td>
+                    </tr>
+                </table>
+            ';
+            $this->writeHTML($html);
+        }
+    }
+
+    $pdf = new TCPDF2();
+    $pdf->SetCreator('Consultare Inc.');
+    $pdf->SetAuthor('InterlinkIQ.com');
+    $pdf->SetSubject('Foreign Supplier Verification Program');
+    $pdf->SetPrintHeader(true);
+    // $pdf->SetMargins(MARGIN_LEFT, MARGIN_TOP, MARGIN_RIGHT, MARGIN_BOTTOM);
+    $pdf->SetFont('helvetica', '', 10);
+    // $pdf->setAutoPageBreak(true, MARGIN_BOTTOM);
+
+    include_once __DIR__ . match($pdfView) {
+        'evaluation_form'       => '/pdfs/evaluation_form.php',
+        'cbp'                   => '/pdfs/cbp_filing_form.php',
+        'importer_information'  => '/pdfs/importer_info.php',
+        'ipr'                   => '/pdfs/ingredient_product_register.php',
+        'activities-worksheet'  => '/pdfs/activities_worksheet.php',
+        default                 => '/pdfs/default.php',
+    };
+}
+
+function debugger($data) {
+    global $debugging;
+
+    if(empty($data)) {
+        exit('Record not found.');
+    }
+    
+    if(isset($debugging) && $debugging) {
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit();
+    }
+}
+
+function txt($value, $default = 'N/A') {
+    return empty($value) ? $default : $value;
 }

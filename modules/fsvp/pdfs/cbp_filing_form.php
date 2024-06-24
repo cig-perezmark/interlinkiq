a5e7ec081b9c76f113a591f35127ae0ff46c88d9
Almario Miano <almariomiano@gmail.com>
2024-06-24 10:00:42 -0500

    updated cbp filing form pdf

--- modules/fsvp/pdfs/cbp_filing_form.php
+++ modules/fsvp/pdfs/cbp_filing_form.php
@@ -25,18 +25,19 @@ $html = $css . '
     <h3 style="text-align:center;">'.$title.'</h3>
     <table>
         <tr>
-            <td>Importer Name: '.($data['importer_name'] ?? '').'</td>
-            <td>Date: '.(date('Ymd', strtotime($data['created_at']))).'</td>
+            <td><strong>Importer Name:</strong> '.($data['importer_name'] ?? '').'</td>
+            <td><strong>Date:</strong> '.(date('Ymd', strtotime($data['created_at']))).'</td>
         </tr>
         <tr>
-            <td colspan="2">Address: '.($data['importer_address']).'</td>
+            <td colspan="2"><strong>Address:</strong> '.($data['importer_address']).'</td>
         </tr>
     </table>
-    <p>Supplier information for FSVP importer
+    <p style="display:none;">Supplier information for FSVP importer
         <br>This Evaluation is to help the Supplier, the entity who received imported food, to ensure that an appropriate FSVP importer has been designated by parties involved in the importation of the food and that the U.S. Customs and Border Protection (CBP).
     </p>
-    <table>
-        <tr>
+    <br>
+    <br><table>
+        <tr style="text-align:center; font-weight:bold;">
             <td>Imported Food(s) / Food Product(s) Information</td>
             <td>Supplier Information</td>
             <td>Determining FSVP Importer</td>
@@ -50,22 +51,22 @@ $html = $css . '
             <td>'.($data['designated_importer'] ?? 'None').'</td>
             <td>'.($data['cbp_entry_filer'] ?? 'None').'</td>
         </tr>
-        <tr>
+        <tr style="display:none;">
             <td colspan="5">*The Supplier identified as the FSVP “importer” in the CBP entry filing. FDA will see as responsible for complying with the FSVP rule.
                 <br>** The definition of FSVP importer is if there is no U.S. owner or consignee at the time of entry and you are the U.S. agent or representative of the foreign owner or consignee.
             </td>
         </tr>
         <tr>
-            <td colspan="3" style="width: 50%;">Reviewed By:</td>
-            <td colspan="2" style="width: 50%;">Date:</td>
+            <td colspan="3" style="width: 50%;"><strong>Reviewed By:</strong></td>
+            <td colspan="2" style="width: 50%;"><strong>Date:</strong></td>
         </tr>
         <tr>
-            <td colspan="3" style="width: 50%;">Approved By:</td>
-            <td colspan="2" style="width: 50%;">Date:</td>
+            <td colspan="3" style="width: 50%;"><strong>Approved By:</strong></td>
+            <td colspan="2" style="width: 50%;"><strong>Date:</strong></td>
         </tr>
     </table>
-    <span>Note: Review and return this form to the Quality Personnel within 24 hours. If the document is not approved, state the reason in the comment section below.</span>
-    <p></p>Comments:
+    <span style="display:none;">Note: Review and return this form to the Quality Personnel within 24 hours. If the document is not approved, state the reason in the comment section below.</span>
+    <br><br><strong>Comments:</strong>
     <br><table>
         <tr>
             <td></td>
